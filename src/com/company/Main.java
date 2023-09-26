7ccd46505b9cf0487312e53b1ef28b32c5312769
Kimia Tuz Zaman <kimia.t.zaman@ieee.org>
2023-09-26 14:44:15 -0500

    variable initialized

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 public class Main {
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    public static final int SHIFT = 3; // Default shift value
 
     public static void main(String[] args) {
 	// write your code here
