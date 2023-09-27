b411eb272d4c681d050ae27732d0de2629052c10
Kimia Tuz Zaman <kimia.t.zaman@ieee.org>
2023-09-26 21:52:13 -0500

    encryptLetter implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,30 @@ public class Main {
         // Convert the StringBuilder to a String and return the encrypted word
         return encryptedWord.toString();
     }
+    public static char encryptLetter(char letter, int shift) {
+        // Check if the character is a lowercase English letter
+        if (Character.isLowerCase(letter)) {
+            // Calculate the index of the original character in the ALPHABET
+            int originalIndex = ALPHABET.indexOf(letter);
 
+            // Calculate the index of the encrypted character after shifting
+            int encryptedIndex = (originalIndex + shift) % ALPHABET.length();
+
+            // Ensure the index wraps around the alphabet
+            if (encryptedIndex < 0) {
+                encryptedIndex += ALPHABET.length();
+            }
+
+            // Get the encrypted character from the ALPHABET
+            char encryptedChar = ALPHABET.charAt(encryptedIndex);
+
+            // Return the encrypted character
+            return encryptedChar;
+        } else {
+            // If the character is not a lowercase English letter, return it unchanged
+            return letter;
+        }
+    }
     private static char encryptLetter(char originalChar, int shift) {
     }
 
