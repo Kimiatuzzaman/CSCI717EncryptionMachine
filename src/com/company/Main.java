a7888aa77f41f094ace191658ac5985a249b4593
Kimia Tuz Zaman <kimia.t.zaman@ieee.org>
2023-09-26 21:57:20 -0500

    encryptLetter implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -42,8 +42,7 @@ public class Main {
             return letter;
         }
     }
-    private static char encryptLetter(char originalChar, int shift) {
-    }
+
 
     public static void main(String[] args) {
 	// write your code here
