580cc46093473cb0c9dc89c64fd5eac6d9f70fa5
Kimia Tuz Zaman <kimia.t.zaman@ieee.org>
2023-09-27 11:36:02 -0500

    main implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     public static final int SHIFT = 3; // Default shift value
@@ -18,6 +20,7 @@ public class Main {
         // Convert the StringBuilder to a String and return the encrypted word
         return encryptedWord.toString();
     }
+
     public static char encryptLetter(char letter, int shift) {
         // Check if the character is a lowercase English letter
         if (Character.isLowerCase(letter)) {
@@ -41,10 +44,37 @@ public class Main {
             // If the character is not a lowercase English letter, return it unchanged
             return letter;
         }
-    }
-
-
+}
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        // Initialize a Scanner to get user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Explain the program to the user
+        System.out.println("Welcome to the CSCI717 Encryption Machine Construction\n" +
+                "The program lets you encrypt a message\n" +
+                "with a key for your recipient to decrypt!\n");
+        System.out.print("Enter your cryptographic key: ");
+        String key = scanner.nextLine();
+
+        // Encrypt the key
+        String encryptedKey = encryptWord(key, SHIFT);
+        System.out.println("Encrypted Key: " + encryptedKey);
+
+        // Ask for the number of words
+        System.out.print("Enter the number of words to encrypt: ");
+        int numWords = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        // Encrypt and print each word
+        for (int i = 0; i < numWords; i++) {
+            System.out.print("Enter word " + (i + 1) + ": ");
+            String word = scanner.nextLine();
+            String encryptedWord = encryptWord(word, SHIFT);
+            System.out.println("Encrypted Word " + (i + 1) + ": " + encryptedWord);
+        }
+        System.out.println("Message fully encrypted. Happy secret messaging!\n");
+        // Close the sc anner
+        scanner.close();
     }
 }
